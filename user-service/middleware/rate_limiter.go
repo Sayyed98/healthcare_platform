1589247a7cc8ed0ceb3fd04b837462447abf49c0
hujaifa <huzaifaanis40@gmail.com>
2026-02-08 11:16:47 +0530

    adding rate limiter in this application

---
+++ user-service/middleware/rate_limiter.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+)
+
+func RateLimiter(rdb *redis.Client, limit int, window time.Duration) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		key := "rate:" + c.ClientIP() + ":" + c.FullPath()
+
+		count, err := rdb.Incr(c, key).Result()
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		if count == 1 {
+			rdb.Expire(c, key, window)
+		}
+
+		if count > int64(limit) {
+			c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{
+				"error": "rate limit exceeded",
+			})
+			return
+		}
+
+		c.Next()
+	}
+}
--- user-service/routes.go
+++ user-service/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"hms/user-service/handler"
 	"hms/user-service/middleware"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
@@ -12,7 +13,7 @@ func registerRoutes(r *gin.Engine, h *handler.UserHandler, redis *redis.Client) 
 	auth := r.Group("/auth")
 	{
 		auth.POST("/register", h.Register)
-		auth.POST("/login", h.Login)
+		auth.POST("/login", middleware.RateLimiter(redis, 5, time.Minute), h.Login)
 		auth.GET("/me", h.Me) // ✅ ADD THIS
 		auth.POST("/logout", middleware.AuthMiddleware(redis), h.Logout)
 	}
